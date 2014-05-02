e814525585a8e44b1334e26711eb12c338dfff75
Duncan Lowrie <dunc@duncster.co.uk>
2014-05-02 14:53:56 +0100

    Added grid lines

---
+++ DGLGameOfLife/DGLGridLinesView.h
@@ -0,0 +1,13 @@
+//
+//  DGLGridLinesView.h
+//  DGLGameOfLife
+//
+//  Created by Duncan Lowrie on 02/05/2014.
+//  Copyright (c) 2014 Duncan Lowrie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGLGridLinesView : UIView
+
+@end
---
+++ DGLGameOfLife/DGLGridLinesView.m
@@ -0,0 +1,31 @@
+//
+//  DGLGridLinesView.m
+//  DGLGameOfLife
+//
+//  Created by Duncan Lowrie on 02/05/2014.
+//  Copyright (c) 2014 Duncan Lowrie. All rights reserved.
+//
+
+#import "DGLGridLinesView.h"
+
+@implementation DGLGridLinesView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
